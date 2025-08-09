94db02aa5bf12581cf3f403cee1a445462e5a895
hironmoy05 <64310411+hironmoy05@users.noreply.github.com>
2025-08-09 23:54:17 +0530

    Add a dynamic page heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, HStack, Show } from '@chakra-ui/react'
+import { Box, Grid, GridItem, HStack, Show } from '@chakra-ui/react'
 import NavBar from './components/NavBar'
 import GameGrid from './components/GameGrid'
 import GenreList from './components/GenreList'
@@ -7,6 +7,7 @@ import type { Genre } from './hooks/useGenre'
 import PlatformSelector from './components/PlatformSelector'
 import type { Platform } from './hooks/usePlatforms'
 import SortSelector from './components/SortSelector'
+import GameHeading from './components/GameHeading'
 import './index.css'
 
 export interface GameQuery {
@@ -38,10 +39,13 @@ const App = () => {
         </GridItem>
       </Show>
       <GridItem area="main">
-        <HStack gap={5} paddingLeft={10} paddingRight={10}>
-          <PlatformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform => setGameQuery({ ...gameQuery, platform }))} />
-          <SortSelector sortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder) => setGameQuery({ ...gameQuery, sortOrder })} />
-        </HStack>
+        <Box paddingLeft={10}>
+          <GameHeading gameQuery={gameQuery} />
+          <HStack gap={5} paddingRight={10}>
+            <PlatformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform => setGameQuery({ ...gameQuery, platform }))} />
+            <SortSelector sortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder) => setGameQuery({ ...gameQuery, sortOrder })} />
+          </HStack>
+        </Box>
         <GameGrid gameQuery={gameQuery} />
       </GridItem>
     </Grid>
