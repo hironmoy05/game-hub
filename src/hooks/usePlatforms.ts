babf6f8a61b348f262d39421def06a0f6b06160a
hironmoy05 <64310411+hironmoy05@users.noreply.github.com>
2025-08-15 20:40:41 +0530

    Implement react query in platroms

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,7 @@
-import useData from "./useData";
+import type { FetchResponse } from './useData';
+import { useQuery } from "@tanstack/react-query";
+import apiClient from "../services/api-client";
+import platforms from '../data/platforms';
 
 export interface Platform {
   id: number;
@@ -6,6 +9,14 @@ export interface Platform {
   slug: string;
 }
 
-const usePlatforms = () => useData<Platform>('/platforms/lists/parents');
+// const usePlatforms = () => useData<Platform>('/platforms/lists/parents');
+const usePlatforms = () => useQuery({
+  queryKey: ['platforms'],
+  queryFn: () =>
+    apiClient.get<FetchResponse<Platform>>('/platforms/lists/parents')
+      .then(res => res.data),
+  staleTime: 24 * 60 * 60 * 1000, // 24 hours
+  initialData: { count: platforms.length, results: platforms } // Initial data from local file
+})
 
 export default usePlatforms;
