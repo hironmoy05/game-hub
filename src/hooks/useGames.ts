5ca3f7bd7540d49eb720343b0e55f6bd5efbf1b6
hironmoy05 <64310411+hironmoy05@users.noreply.github.com>
2025-08-15 20:59:32 +0530

    Refactoring fetch games with react query

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,6 @@
-import useData from "./useData"
 import type { GameQuery } from '../App';
+import { useQuery } from "@tanstack/react-query";
+import apiClient, { type FetchResponse } from "../services/api-client";
 
 export interface Platform {
   id: number,
@@ -16,12 +17,26 @@ export interface Game {
   rating_top: number,
 }
 
-const useGames = (gameQuery: GameQuery) => useData<Game>("games", 
-  {params: {
-    genres: gameQuery?.genre?.id, 
-    platforms: gameQuery?.platform?.id, 
-    ordering: gameQuery.sortOrder,
-    search: gameQuery.searchText}},
-  [gameQuery])
+const useGames = (gameQuery: GameQuery) =>
+  useQuery<FetchResponse<Game>, Error>({
+    queryKey: ['games', gameQuery],
+    queryFn: () =>
+      apiClient.get<FetchResponse<Game>>('games', {
+        params: {
+        genres: gameQuery?.genre?.id, 
+        parent_platforms: gameQuery?.platform?.id, 
+        ordering: gameQuery.sortOrder,
+        search: gameQuery.searchText}
+      })
+      .then(res => res.data)
+  })
+  
+  // useData<Game>("games", 
+  // {params: {
+  //   genres: gameQuery?.genre?.id, 
+  //   platforms: gameQuery?.platform?.id, 
+  //   ordering: gameQuery.sortOrder,
+  //   search: gameQuery.searchText}},
+  // [gameQuery])
 
 export default useGames
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,5 @@
-import type { FetchResponse } from './useData';
 import { useQuery } from "@tanstack/react-query";
-import apiClient from "../services/api-client";
+import apiClient, { type FetchResponse } from "../services/api-client";
 import platforms from '../data/platforms';
 
 export interface Platform {
