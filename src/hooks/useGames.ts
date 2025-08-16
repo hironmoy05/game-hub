e531f62a588c13ecb311e28a40dbf82c575c3bc0
hironmoy05 <64310411+hironmoy05@users.noreply.github.com>
2025-08-16 20:30:30 +0530

    implement infinite queries

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,5 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
 import type { GameQuery } from '../App';
-import { useQuery } from "@tanstack/react-query";
 import APIClient, { type FetchResponse } from "../services/api-client";
 import type { Platform } from './usePlatforms';
 
@@ -16,16 +16,19 @@ export interface Game {
 const apiClient = new APIClient<Game>("/games")
 
 const useGames = (gameQuery: GameQuery) =>
-  useQuery<FetchResponse<Game>, Error>({
+  useInfiniteQuery<FetchResponse<Game>, Error>({
     queryKey: ['games', gameQuery],
-    queryFn: () => apiClient.getAll({
+    queryFn: ({pageParam = 1}) => apiClient.getAll({
         params: {
           genres: gameQuery?.genre?.id,
           parent_platforms: gameQuery?.platform?.id,
           ordering: gameQuery?.sortOrder,
-          search: gameQuery?.searchText
+          search: gameQuery?.searchText,
+          page: pageParam
         }
-      })
+      }),
+      getNextPageParam: (lastPage, allPages) => 
+        lastPage.next ? allPages.length + 1 : undefined
   })
 
 export default useGames
